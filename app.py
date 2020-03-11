073d6d92774ae399a1a6c6b1fed017e9ee1f99f7
JonathanLima25 <jonathan.lima.sjc23@gmail.com>
2020-03-10 21:30:34 -0300

    adicionando json com padrão

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify, current_app
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Not Found", 404, {'X-hello':'world'}
+    response = current_app.make_response("Hello")
+    response.set_cookie('name', value='Jonathan')   
+    return response
 
 @app.route('/xml/')
 def xml():
     return "<use name='Jonathan'/>", {'Content-type':'application/xml'}
+
+@app.route('/json/')
+def json():
+    return jsonify(user={'name': 'Jonathan'})
